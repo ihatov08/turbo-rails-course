c27dea06635dd4a0b39f493b9a040124a337aa6e
tomoya yoshida <ihatov08@gmail.com>
2026-04-06 00:33:18 +0000

    コースの作成

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -4,6 +4,7 @@ class Admin::CoursesController < ApplicationController
   end
 
   def new
+    @course = Course.new
   end
 
   def edit
@@ -13,11 +14,23 @@ class Admin::CoursesController < ApplicationController
   end
 
   def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to admin_courses_path, notice: "コースが作成されました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name)
   end
 end
